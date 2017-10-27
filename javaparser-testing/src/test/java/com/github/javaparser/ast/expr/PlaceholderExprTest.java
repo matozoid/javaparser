74672bf6e689a4a750e9831d76fc76a240838d67
Danny van Bruggen <hexagonaal@gmail.com>
2017-10-27 23:34:40 +0200

    Improve tests

--- javaparser-testing/src/test/java/com/github/javaparser/ast/expr/PlaceholderExprTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/expr/PlaceholderExprTest.java
@@ -1,18 +1,16 @@
 package com.github.javaparser.ast.expr;
 
-import com.github.javaparser.ast.body.BodyDeclaration;
 import org.junit.Test;
 
-import static com.github.javaparser.JavaParser.parseBodyDeclaration;
 import static com.github.javaparser.JavaParser.parseExpression;
-import static org.junit.Assert.*;
+import static com.github.javaparser.utils.TestUtils.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlaceholderExprTest {
     @Test
     public void test() {
         Expression expression = parseExpression("1+`a`+3");
-        System.out.println(expression);
+        assertInstanceOf(PlaceholderExpr.class, expression.asBinaryExpr().getLeft().asBinaryExpr().getRight());
+        assertEquals("a", expression.findFirst(PlaceholderExpr.class).get().getNameAsString());
     }
-
-
 }
--- javaparser-testing/src/test/java/com/github/javaparser/ast/stmt/PlaceholderStmtTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/stmt/PlaceholderStmtTest.java
@@ -1,15 +1,17 @@
 package com.github.javaparser.ast.stmt;
 
-import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import org.junit.Test;
 
 import static com.github.javaparser.JavaParser.parseBodyDeclaration;
+import static com.github.javaparser.utils.TestUtils.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlaceholderStmtTest {
     @Test
     public void test() {
-        BodyDeclaration<?> bodyDeclaration = parseBodyDeclaration("int x() { int a; `abc` print(1);}");
-        System.out.println(bodyDeclaration);
+        MethodDeclaration methodDeclaration = parseBodyDeclaration("int x() { int a; `abc` print(1);}").asMethodDeclaration();
+        assertInstanceOf(PlaceholderStmt.class, methodDeclaration.asMethodDeclaration().getBody().get().getStatement(1));
+        assertEquals("abc", methodDeclaration.findFirst(PlaceholderStmt.class).get().getNameAsString());
     }
-
 }
