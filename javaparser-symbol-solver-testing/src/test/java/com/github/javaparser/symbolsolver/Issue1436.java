a4793cdadf992b9b9b3edd86ecdccae0a58fe94b
Danny van Bruggen <hexagonaal@gmail.com>
2018-02-22 21:01:20 +0100

    Better formatting

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue1436.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue1436.java
@@ -4,7 +4,6 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseStart;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import org.junit.Test;
@@ -16,18 +15,20 @@ public class Issue1436 {
     public void test() {
         JavaParser javaParser = new JavaParser(new ParserConfiguration().setSymbolResolver(new JavaSymbolSolver(new ReflectionTypeSolver())));
 
-        CompilationUnit cu = javaParser.parse(ParseStart.COMPILATION_UNIT, provider("        abstract class TypeIfc {\n" +
-                "        }\n" +
-                "        class TypeA {\n" +
-                "            void doSomething(TypeIfc typeIfc) {\n" +
-                "            }\n" +
-                "        }\n" +
-                "\n" +
-                "        class B { void x() {TypeA obj = new TypeA();\n" +
-                "        obj.doSomething(new TypeIfc() {\n" +
-                "        });}}\n" +
-                "\n")).getResult().get();
+        CompilationUnit cu = javaParser.parse(ParseStart.COMPILATION_UNIT, provider("abstract class TypeIfc {}" +
+                "class TypeA {" +
+                "  void doSomething(TypeIfc typeIfc) {" +
+                "  }" +
+                "}" +
 
-        cu.findAll(ObjectCreationExpr.class).forEach(Expression::calculateResolvedType);
+                "class B {" +
+                "  void x() {" +
+                "    TypeA obj = new TypeA();" +
+                "    obj.doSomething(new TypeIfc() {" +
+                "    });" +
+                "  }" +
+                "}")).getResult().get();
+
+        cu.findAll(ObjectCreationExpr.class).forEach(oce -> System.out.println(oce.calculateResolvedType()));
     }
 }
